4a1046a5b524f538671b65e8c69fae5e86c8c0c8
José Divino <josedivinooficial02@gmail.com>
2025-01-21 23:17:41 -0300

    Adds categories create and store route

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Http\Controllers\Category\CategoryController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::prefix("categories")->group(function () {
+    Route::get("/", [CategoryController::class, "create"])->name("categories.create");
+    Route::post("/", [CategoryController::class, "store"])->name("categories.store");
 });
